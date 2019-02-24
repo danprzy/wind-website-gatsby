adfa61127a64d339bb05f7bf0cf55d0600c280f6
Danuta Przydrozna <danprzy@gmail.com>
2019-02-24 23:07:28 +0100

    add Testimonials and Clients without js and gatsby image

--- src/components/Home/Clients.js
+++ src/components/Home/Clients.js
@@ -4,18 +4,67 @@ import Title from "../Globals/Title";
 
 export default function info() {
   return (
-    <section className="py-5">
-      <div className="container">
-        <Title title="our story" />
+    <section classNameName="py-5">
+      <div className="container box-clients">
         <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">Clients</p>
-            <Link to="/about">
-              <button className="btn text-uppercase btn-yellow">
-                about page
-              </button>
-            </Link>
+          <div className="col-md-12 section-heading text-center">
+            <h2 className="to-animate">Our Clients</h2>
           </div>
+        </div>
+        <div className="row " id="carousel">
+          <ul className="to-animate">
+            <li>
+              <div className="client_img cez img-responsive" />
+            </li>
+            <li>
+              <div className="client_img greenbear img-responsive" />
+            </li>
+            <li>
+              <div className="client_img renesola img-responsive" />
+            </li>
+            <li>
+              <div className="client_img eesti img-responsive" />
+            </li>
+            <li>
+              <div className="client_img cee img-responsive" />
+            </li>
+            <li>
+              <div className="client_img baltcap img-responsive" />
+            </li>
+            <li>
+              <div className="client_img ge img-responsive" />
+            </li>
+            <li>
+              <div className="client_img mep img-responsive" />
+            </li>
+            <li>
+              <div className="client_img bos img-responsive" />
+            </li>
+            <li>
+              <div className="client_img ebrd img-responsive" />
+            </li>
+            <li>
+              <div className="client_img laing img-responsive" />
+            </li>
+            <li>
+              <div className="client_img alior img-responsive" />
+            </li>
+            <li>
+              <div className="client_img polenergia img-responsive" />
+            </li>
+            <li>
+              <div className="client_img enea img-responsive" />
+            </li>
+            <li>
+              <div className="client_img strabag img-responsive" />
+            </li>
+            <li>
+              <div className="client_img cgn img-responsive" />
+            </li>
+            <li>
+              <div className="client_img tommaszt img-responsive" />
+            </li>
+          </ul>
         </div>
       </div>
     </section>
--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -4,18 +4,72 @@ import Title from "../Globals/Title";
 
 export default function info() {
   return (
-    <section className="py-5">
+    <section id="fh5co-testimonials" data-section="testimonials">
       <div className="container">
-        <Title title="our story" />
         <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">Hello from Testimonials</p>
-            <Link to="/about">
-              <button className="btn text-uppercase btn-yellow">
-                about page
-              </button>
-            </Link>
+          <div className="col-md-12 section-heading text-center">
+            <h2 className="to-animate">Testimonials</h2>
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 subtext to-animate" />
+            </div>
           </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4 col-md-offset-2">
+            <div className="box-testimony">
+              <blockquote className="to-animate-2">
+                <p>
+                  &ldquo;Wind Prospect is a robust and reliable technical
+                  advisor. It is a solid team of experts working in an
+                  exceptionally efficient manner, while bringing and exposing
+                  value of the project. As a long-term client of Wind Prospect I
+                  fully recommend them as professional partner.&rdquo;
+                </p>
+              </blockquote>
+              <div className="author to-animate">
+                <p>
+                  JEAN-CLAUDE MOUSTACAKIS <br />
+                  <a href="http://www.greenbearcorp.com/" target="_blank">
+                    Green Bear Corporation Poland Sp. z o.o.
+                  </a>
+                  <span className="subtext">CEO</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="box-testimony">
+              <blockquote className="to-animate-2">
+                <p>
+                  &ldquo;We would thoroughly recommend Wind Prospect as a
+                  quality partner in all aspects of wind project development,
+                  construction, operation and consultancy.&rdquo;
+                </p>
+              </blockquote>
+              <div className="author to-animate">
+                <p>
+                  <a href="https://www.edf-re.com/" target="_blank">
+                    EDF Renewables
+                  </a>{" "}
+                  <span className="subtext">CEO</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="col-md-4">
+                  <div className="box-testimony">
+                    <blockquote className="to-animate-2">
+                      <p>&ldquo;Vivamus eget iaculis ipsum, non ultricies lectus. Vivamus in ligula sit amet mi finibus rutrum. Interdum et malesuada fames ac ante ipsum primis in faucibus. &rdquo;</p>
+                    </blockquote>
+                    <div className="author to-animate">
+                      <figure><img src="images/person3_.jpg" alt="Person"></figure>
+                        <p>
+                          Adam Nowak <a href="#" target="_blank">Firma XXX</a> <span className="subtext">Creative Director</span>
+                        </p>
+                        </div>
+                    </div>
+                  </div>
+  -->*/}
         </div>
       </div>
     </section>
